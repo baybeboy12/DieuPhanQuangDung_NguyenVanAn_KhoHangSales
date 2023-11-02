90787aedadb065886c99dc0d0b5360bdc664fe10
NguyenVanAn2002 <98084517+NguyenVanAn2002@users.noreply.github.com>
2023-11-02 18:30:19 +0700

    Update App.js
    demo

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  Pressable,
+} from "react-native";
+import * as React from "react";
+
+import Icon from "react-native-vector-icons/FontAwesome";
+import { Image } from "react-native";
+import SignIn from "./Screen/SignIn";
+import SignUp from "./Screen/SignUp";
+// import { useState, useEffect } from "react";
+// import { useRoute } from "@react-navigation/native";
+// import { useNavigation } from "@react-navigation/native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <SignIn/>
+    <SignUp/>
   );
 }
 
@@ -14,7 +28,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
